565264ad7060ef9cd309b6e68c5f5a97926c87fa
Trung Hậu <crud5204@gmail.com>
2022-04-29 09:03:55 +0700

    -

---
+++ backend/backend-e-learning/src/main/java/com/group6/java16/cybersoft/role/repository/ELGroupRepository.java
@@ -0,0 +1,19 @@
+package com.group6.java16.cybersoft.role.repository;
+
+import java.util.UUID;
+
+import com.group6.java16.cybersoft.role.model.ELGroup;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ELGroupRepository extends JpaRepository<ELGroup, UUID> {
+    @Query(value = "Select r from ELGroup r where r.name like %:name% ")
+    Page<ELGroup> searchByName(@Param("name") String name, Pageable pageable);
+
+}
