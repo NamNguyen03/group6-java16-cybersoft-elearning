39992a3b8d56e33da45fe48b4a12da4b2426d24c
Trung Hậu <crud5204@gmail.com>
2022-05-05 14:31:06 +0700

    fix validate dto

--- backend/backend-e-learning/src/main/java/com/group6/java16/cybersoft/role/dto/RoleDTO.java
+++ backend/backend-e-learning/src/main/java/com/group6/java16/cybersoft/role/dto/RoleDTO.java
@@ -1,5 +1,10 @@
 package com.group6.java16.cybersoft.role.dto;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+import com.group6.java16.cybersoft.role.validation.annotation.UniqueRoleName;
+
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,8 +12,12 @@ import lombok.Setter;
 @Setter
 public class RoleDTO {
 	
+	@Size(min = 3, max = 100, message = "{role.name.size}")
+	@UniqueRoleName(message = "{role.name.existed}")
+	@NotBlank(message = "{role.name.not-blank}")
 	private String name;
 	
+	@NotBlank(message = "{role.description.not-blank}")
 	private String description;
 
 }
