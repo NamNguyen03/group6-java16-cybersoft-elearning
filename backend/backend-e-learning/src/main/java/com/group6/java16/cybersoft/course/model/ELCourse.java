ab07076950350035083892ca85fa0b9e7ff0e99b
Trung Hậu <crud5204@gmail.com>
2022-05-13 17:16:00 +0700

    alter table course

--- backend/backend-e-learning/src/main/java/com/group6/java16/cybersoft/course/model/ELCourse.java
+++ backend/backend-e-learning/src/main/java/com/group6/java16/cybersoft/course/model/ELCourse.java
@@ -1,6 +1,8 @@
 package com.group6.java16.cybersoft.course.model;
 
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.Table;
 
 import com.group6.java16.cybersoft.common.model.BaseEntity;
@@ -17,11 +19,34 @@ import lombok.experimental.SuperBuilder;
 @Entity
 @Table(name = "el_course")
 public class ELCourse extends BaseEntity {
-	
+
 	private String courseName;
-	
+
 	private int courseTime;
-	
+
 	private String description;
+	
+	private String img;
+	
+	private String level;
+	
+	private float star_avg;
+	
+	private int total_star;
+	
+	private int total_rating;
+	
+	private String skill_1;
+	
+	private String skill_2;
+	
+	private String skill_3;
+	
+	private String skill_4;
+	
+	private String skill_5;
+
+	@Enumerated(EnumType.STRING)
+	private CategoryEnum category;
 
 }
