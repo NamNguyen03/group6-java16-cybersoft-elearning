afc9b93093e30444e8fa4527940fa54cd8860ac1
Nam Nguyen <nam03031999@gmail.com>
2022-05-19 01:21:22 -0400

    create table comment rating

--- backend/backend-e-learning/src/main/java/com/group6/java16/cybersoft/feedback/model/ELStatusComment.java
+++ backend/backend-e-learning/src/main/java/com/group6/java16/cybersoft/feedback/model/ELStatusComment.java
@@ -1,36 +1,41 @@
 package com.group6.java16.cybersoft.feedback.model;
 
+import com.group6.java16.cybersoft.common.model.BaseEntity;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.group6.java16.cybersoft.common.model.BaseEntity;
-import com.group6.java16.cybersoft.course.model.ELLesson;
+import com.group6.java16.cybersoft.course.model.ELCourse;
 import com.group6.java16.cybersoft.user.model.ELUser;
 
 import lombok.*;
 import lombok.experimental.SuperBuilder;
+
 
 @Getter
 @Setter
 @NoArgsConstructor
 @SuperBuilder
 @Entity
-@Table(name = "el_comment")
-public class ELComment extends BaseEntity  {
-    private String content;
+@Table(name = "el_status_comment")
+public class ELStatusComment extends BaseEntity {
 
-	@JsonIgnore
+    @Enumerated(EnumType.STRING)
+    private EnumStatusComment status;
+
+    @JsonIgnore
 	@ManyToOne(cascade = { CascadeType.MERGE, CascadeType.PERSIST })
 	@JoinColumn(name = "user_id",referencedColumnName = "id")
     private ELUser user;
 
     @JsonIgnore
 	@ManyToOne(cascade = { CascadeType.MERGE, CascadeType.PERSIST })
-	@JoinColumn(name = "lesson_id",referencedColumnName = "id")
-    private ELLesson lesson;
+	@JoinColumn(name = "course_id",referencedColumnName = "id")
+    private ELCourse course;
 }
--- backend/backend-e-learning/src/main/resources/db/migration/V13__feedback.sql
+++ backend/backend-e-learning/src/main/resources/db/migration/V13__feedback.sql
@@ -7,11 +7,11 @@ CREATE TABLE el_comment (
 	VERSION int4 NOT NULL,
 	content VARCHAR ( 255 ),
 	user_id VARCHAR ( 36 ) NOT NULL,
-    course_id VARCHAR ( 36 ) NOT NULL,
+    lesson_id VARCHAR ( 36 ) NOT NULL,
     PRIMARY KEY ( ID ),
-    CONSTRAINT FK_COMMENT_COURSE
-        FOREIGN KEY(course_id) 
-        REFERENCES el_course(id),
+    CONSTRAINT FK_COMMENT_LESSON
+        FOREIGN KEY(lesson_id) 
+        REFERENCES el_lesson(id),
     CONSTRAINT FK_COMMENT_USER
 	    FOREIGN KEY(user_id) 
   	    REFERENCES el_user(id)
@@ -26,12 +26,32 @@ CREATE TABLE el_rating (
 	VERSION int4 NOT NULL,
 	value int4 NOT NULL,
 	user_id VARCHAR ( 36 ) NOT NULL,
-    course_id VARCHAR ( 36 ) NOT NULL,
+    lesson_id VARCHAR ( 36 ) NOT NULL,
     PRIMARY KEY ( ID ),
-    CONSTRAINT FK_RATING_COURSE
-        FOREIGN KEY(course_id) 
-        REFERENCES el_course(id),
+    CONSTRAINT FK_RATING_LESSON
+        FOREIGN KEY(lesson_id) 
+        REFERENCES el_lesson(id),
     CONSTRAINT FK_RATING_USER
         FOREIGN KEY(user_id) 
         REFERENCES el_user(id)
 );
+
+CREATE TABLE el_status_comment (
+	ID VARCHAR ( 36 ) NOT NULL,
+	created_at TIMESTAMP,
+	created_by VARCHAR ( 36 ),
+	last_modified_at TIMESTAMP,
+	last_modified_by VARCHAR ( 36 ),
+	VERSION int4 NOT NULL,
+	status VARCHAR ( 36 ) NOT NULL,
+	user_id VARCHAR ( 36 ) NOT NULL,
+    course_id VARCHAR ( 36 ) NOT NULL,
+    PRIMARY KEY ( ID ),
+    CONSTRAINT FK_STATUS_COMMENT_COURSE
+        FOREIGN KEY(course_id) 
+        REFERENCES el_course(id),
+    CONSTRAINT FK_SATUS_COMMENT_USER
+        FOREIGN KEY(user_id) 
+        REFERENCES el_user(id)
+);
+
