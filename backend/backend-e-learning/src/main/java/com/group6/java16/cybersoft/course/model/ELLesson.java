e10d7c89c6b48a73586840f23047da5c576d928e
Trung Hậu <crud5204@gmail.com>
2022-05-23 16:11:04 +0700

    alter table el_lesson

--- backend/backend-e-learning/src/main/java/com/group6/java16/cybersoft/course/model/ELLesson.java
+++ backend/backend-e-learning/src/main/java/com/group6/java16/cybersoft/course/model/ELLesson.java
@@ -10,6 +10,7 @@ import javax.persistence.Table;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.group6.java16.cybersoft.common.model.BaseEntity;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -31,6 +32,14 @@ public class ELLesson extends BaseEntity{
 	
 	@Column(name = "content",nullable = false)
 	private String content;
+	
+	private float starAvg;
+	
+	// totalStar = sum (star.value)
+	private int totalStar;
+	// count rating
+	private int totalRating;
+
 	
 	@JsonIgnore
 	@ManyToOne(cascade = { CascadeType.MERGE, CascadeType.PERSIST })
